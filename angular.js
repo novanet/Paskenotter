bf2872bc3158480079ca90aea418ab57c5014004
Hallstein Brøtan <Hallstein Brtan>
2017-04-03 10:42:46 +0200

    http handling

--- angular.js
+++ angular.js
@@ -15,12 +15,10 @@
         vm.error = null;
         vm.hasSubmitted = false;
         vm.isSubmitting = false;       
-        vm.submit = submit;     
-
-        vm.nuts = getNuts();           
+        vm.submit = submit;             
               
         function submit(){
-            if(!vm.answer.user || !vm.answer.email || !vm.answer.company){
+            if(!vm.answer.name || !vm.answer.email || !vm.answer.company){
                 vm.error = "Du må fylle ut navn, epost og firma";
                 return;
             }                               
@@ -28,9 +26,12 @@
             vm.error = null;
             vm.isSubmitting = true;
             
-            dataservice.postAnswer(vm.answer)
-                .then(function(result){
+            dataservice.post(vm.answer)
+                .then(function(){
                     vm.hasAnswered = true;
+                })
+                .catch(function(){
+                    vm.error = "Det har skjedd en feil ved levering av svarene. Vennligst prøv på nytt.";
                 })
                 .finally(function(){
                     vm.isSubmitting = false;    
@@ -40,62 +41,27 @@
 
     function dataservice($q, $http) {
         return {
-            postAnswer : postAnswer   
+            post : post   
         }
       
-       function postAnswer(answer) {                              
+        function post(data) {
             return $q(function (resolve, reject) {
-                $http.post('http://easterquiz-api.azurewebsites.net/api/answers', answer)
-                    .then(function (response) {
-                        resolve(response.data);
-                    });
-            });
-        }       
-    }
+                $http.post('http://easterquiz-api.azurewebsites.net/api/answers', data)
+                    .then(saveComplete)
+                    .catch(saveFailed);
 
-    function getNuts(){
-        return [
-                { "id": 1, "q": "Rebus 1", "domestic": true },
-                { "id": 1, "q": "Rebus 2", "domestic": true },
-                { "id": 1, "q": "Rebus 3", "domestic": true },
-                { "id": 1, "q": "Rebus 4", "domestic": true },
-                { "id": 1, "q": "Rebus 5", "domestic": true },
-                { "id": 1, "q": "Rebus 6", "domestic": true },
-                { "id": 1, "q": "Rebus 7", "domestic": true },
-                { "id": 1, "q": "Rebus 8", "domestic": true },
-                { "id": 1, "q": "Rebus 9", "domestic": true },
-                { "id": 1, "q": "Rebus 10", "domestic": true },
-                { "id": 1, "q": "Rebus 11", "domestic": true },
-                { "id": 1, "q": "Rebus 12", "domestic": true },
-                { "id": 1, "q": "Rebus 13", "domestic": true },
-                { "id": 1, "q": "Rebus 14", "domestic": true },
-                { "id": 1, "q": "Rebus 15", "domestic": true },
-                { "id": 1, "q": "Rebus 16", "domestic": true },
-                { "id": 1, "q": "Rebus 17", "domestic": true },
-                { "id": 1, "q": "Rebus 18", "domestic": true },
-                { "id": 1, "q": "Rebus 19", "domestic": true },
-                { "id": 1, "q": "Rebus 20", "domestic": false },
-                { "id": 1, "q": "Rebus 21", "domestic": false },
-                { "id": 1, "q": "Rebus 22", "domestic": false },
-                { "id": 1, "q": "Rebus 23", "domestic": false },
-                { "id": 1, "q": "Rebus 24", "domestic": false },
-                { "id": 1, "q": "Rebus 25", "domestic": false },
-                { "id": 1, "q": "Rebus 26", "domestic": false },
-                { "id": 1, "q": "Rebus 27", "domestic": false },
-                { "id": 1, "q": "Rebus 28", "domestic": false },
-                { "id": 1, "q": "Rebus 29", "domestic": false },
-                { "id": 1, "q": "Rebus 30", "domestic": false },
-                { "id": 1, "q": "Rebus 31", "domestic": false },
-                { "id": 1, "q": "Rebus 32", "domestic": false },
-                { "id": 1, "q": "Rebus 33", "domestic": false },
-                { "id": 1, "q": "Rebus 34", "domestic": false },
-                { "id": 1, "q": "Rebus 35", "domestic": false },
-                { "id": 1, "q": "Rebus 36", "domestic": false },
-                { "id": 1, "q": "Rebus 37", "domestic": false },
-                { "id": 1, "q": "Rebus 38 ewwefwe wefwe", "domestic": false },
-                { "id": 1, "q": "Rebus 39", "domestic": false },
-                { "id": 1, "q": "Rebus 40", "domestic": false },
-                { "id": 1, "q": "Rebus 41", "domestic": false },
-            ];
-    }
+                function saveComplete(response) {
+                    if (!response || !response.data.success) {
+                        reject(response.data.errorCode);
+                    } else {
+                        resolve(response.data.result);
+                    }
+                }
+
+                function saveFailed() {
+                    reject();
+                }
+            });
+        }
+    }    
 })();
